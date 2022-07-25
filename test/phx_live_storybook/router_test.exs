4d5e0e5a5b29a8ce48e9b10646b52fc1ef4d9ffc
Christian Blavier <cblavier@gmail.com>
2022-07-25 18:29:48 +0200

    more router testing

---
+++ test/phx_live_storybook/router_test.exs
@@ -0,0 +1,47 @@
+defmodule PhxLiveStorybook.RouterTest do
+  use ExUnit.Case, async: true
+
+  import Phoenix.ConnTest
+
+  alias PhxLiveStorybook.TestRouter.Helpers, as: Routes
+
+  test "generates helper for home" do
+    assert Routes.live_storybook_path(build_conn(), :home) == "/storybook"
+  end
+
+  test "raises when backend_module is missing" do
+    assert_raise(RuntimeError, fn ->
+      defmodule NoBackendModuleRouter do
+        use Phoenix.Router
+        import PhxLiveStorybook.Router
+
+        pipeline :browser do
+          plug(:fetch_session)
+        end
+
+        scope "/", ThisWontBeUsed, as: :this_wont_be_used do
+          pipe_through(:browser)
+          live_storybook("/storybook", otp_app: :phx_live_storybook)
+        end
+      end
+    end)
+  end
+
+  test "raises when otp_app is missing" do
+    assert_raise(RuntimeError, fn ->
+      defmodule NoBackendModuleRouter do
+        use Phoenix.Router
+        import PhxLiveStorybook.Router
+
+        pipeline :browser do
+          plug(:fetch_session)
+        end
+
+        scope "/", ThisWontBeUsed, as: :this_wont_be_used do
+          pipe_through(:browser)
+          live_storybook("/storybook", backend_module: PhxLiveStorybook.TestStorybook)
+        end
+      end
+    end)
+  end
+end
