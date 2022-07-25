e6616792fdd4ea80af1adc3bad88e48d362c5b47
Christian Blavier <cblavier@gmail.com>
2022-07-25 20:05:20 +0200

    new entry_test.exs

---
+++ test/phx_live_storybook/entry_test.exs
@@ -0,0 +1,30 @@
+defmodule PhxLiveStorybook.EntryTest do
+  use ExUnit.Case, async: true
+
+  test "component entry default behaviors" do
+    defmodule MyEntry do
+      use PhxLiveStorybook.Entry, :component
+      def component, do: nil
+      def function, do: nil
+    end
+
+    assert MyEntry.name() == "My Entry"
+    assert MyEntry.variations() == []
+    assert MyEntry.storybook_type() == :component
+    assert MyEntry.description() == ""
+    assert MyEntry.icon() == nil
+  end
+
+  test "live_component entry default behaviors" do
+    defmodule MyLiveEntry do
+      use PhxLiveStorybook.Entry, :live_component
+      def component, do: nil
+    end
+
+    assert MyLiveEntry.name() == "My Live Entry"
+    assert MyLiveEntry.variations() == []
+    assert MyLiveEntry.storybook_type() == :live_component
+    assert MyLiveEntry.description() == ""
+    assert MyLiveEntry.icon() == nil
+  end
+end
