e8559d650b771e89819fdaaba6d37e9501825b0b
Matthieu Chabert <hey@matthieuchabert.com>
2022-08-18 16:36:09 +0200

    added tab and enter functionality to navigate within the search modal

--- lib/phx_live_storybook/live/search.ex
+++ lib/phx_live_storybook/live/search.ex
@@ -4,8 +4,6 @@ defmodule PhxLiveStorybook.Search do
 
   alias Phoenix.LiveView.JS
 
-  # alias PhxLiveStorybook.{ComponentEntry, FolderEntry}
-
   def mount(socket) do
     {:ok, socket}
   end
@@ -21,9 +19,8 @@ defmodule PhxLiveStorybook.Search do
     }
   end
 
-  defp hide_search(js \\ %JS{}) do
-    IO.inspect("JS")
-    JS.add_class(js, "lsb-hidden", transition: "ease-out", to: "#search-container")
+  def handle_event("navigate", %{"path" => path}, socket) do
+   {:noreply, push_patch(socket, to: path)}
   end
 
 
@@ -45,20 +42,17 @@ defmodule PhxLiveStorybook.Search do
         -->
         <div class="lsb-mx-auto lsb-max-w-xl lsb-transform lsb-divide-y lsb-divide-gray-100 lsb-overflow-hidden lsb-rounded-xl lsb-bg-white lsb-shadow-2xl lsb-ring-1 lsb-ring-black lsb-ring-opacity-5 lsb-transition-all">
           <div class="relative">
-            <!-- Heroicon name: solid/search -->
-            <svg class="lsb-pointer-events-none lsb-absolute lsb-top-3.5 lsb-left-4 lsb-h-5 lsb-w-5 lsb-text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-            </svg>
+            <i class="fal fa-search lsb lsb-pointer-events-none lsb-absolute lsb-top-3.5 lsb-left-4 lsb-h-5 lsb-w-5 lsb-text-gray-400"></i>
             <input type="text" class="lsb-h-12 lsb-w-full lsb-border-0 lsb-bg-transparent lsb-pl-11 lsb-pr-4 lsb-text-gray-800 lsb-placeholder-gray-400 lsb-focus:ring-0 sm:text-sm" placeholder="Search..." role="combobox" aria-expanded="false" aria-controls="options">
           </div>
 
           <!-- Results, show/hide based on command palette state -->
           <ul class="lsb-max-h-72 lsb-scroll-py-2 lsb-overflow-y-auto lsb-py-2 lsb-text-sm lsb-text-gray-800">
             <!-- Active: "bg-indigo-600 text-white" -->
-              <%= for entry <- @entries do %>
+              <%= for {entry, i} <- Enum.with_index(@entries, 2) do %>
                 <% entry_path =  @root_path <> entry.storybook_path %>
 
-                <li class="lsb-cursor-default lsb-select-none lsb-px-4 lsb-py-2">
+                <li class="lsb-cursor-default lsb-select-none lsb-px-4 lsb-py-2 focus:lsb-border-blue-400" tabindex={i}>
                   <%= live_patch(entry.name, to: entry_path, class: "lsb group-hover:lsb-text-indigo-600") %>
                 </li>
               <% end %>
--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -4904,6 +4904,14 @@ within:
     mounted() {
       const searchContainer = document.querySelector("#search-container");
       window.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+          if (e.target == document.activeElement) {
+            const link = e.target.children[0];
+            this.pushEventTo("#search-container", "navigate", { path: link.pathname });
+            searchContainer.classList.add("lsb-ease-in");
+            searchContainer.classList.add("lsb-hidden");
+          }
+        }
         if (e.metaKey && e.key === "k") {
           searchContainer.classList.add("lsb-ease-out");
           searchContainer.classList.remove("lsb-hidden");
