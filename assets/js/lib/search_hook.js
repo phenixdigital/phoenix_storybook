195667950804fe49efa9df4cab8cc83c99d8234c
Matthieu Chabert <hey@matthieuchabert.com>
2022-08-18 16:36:09 +0200

    added tab and enter functionality to navigate within the search modal

--- assets/js/lib/search_hook.js
+++ assets/js/lib/search_hook.js
@@ -3,6 +3,16 @@ export const SearchHook = {
       const searchContainer = document.querySelector("#search-container");
 
       window.addEventListener('keydown', (e) => {
+         if(e.key === 'Enter'){
+            if (e.target == document.activeElement) {
+               const link = e.target.children[0]
+               this.pushEventTo("#search-container", "navigate", {path: link.pathname});
+
+               searchContainer.classList.add("lsb-ease-in");
+               searchContainer.classList.add("lsb-hidden");
+            }
+         }
+
          if(e.metaKey && e.key === 'k'){
             searchContainer.classList.add("lsb-ease-out");
             searchContainer.classList.remove("lsb-hidden");
@@ -17,6 +27,7 @@ export const SearchHook = {
       searchContainer.addEventListener('click', (e) => {
          searchContainer.classList.add("lsb-ease-in");
          searchContainer.classList.add("lsb-hidden");
-   })}
+      })
+}
  };
  
