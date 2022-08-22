8ab62477b1f66b5d75b0826cb84bebcbc18e5568
Matthieu Chabert <hey@matthieuchabert.com>
2022-08-22 16:35:30 +0200

    added search input to filter components

--- assets/js/lib/search_hook.js
+++ assets/js/lib/search_hook.js
@@ -1,6 +1,8 @@
 export const SearchHook = {
    mounted() {
       const searchContainer = document.querySelector("#search-container");
+      const searchList = document.querySelector("#search-list");
+      const searchInput = document.querySelector("#search-container input");
 
       window.addEventListener('keydown', (e) => {
          if(e.key === 'Enter'){
@@ -8,25 +10,22 @@ export const SearchHook = {
                const link = e.target.children[0]
                this.pushEventTo("#search-container", "navigate", {path: link.pathname});
 
-               searchContainer.classList.add("lsb-ease-in");
-               searchContainer.classList.add("lsb-hidden");
+               this.liveSocket.execJS(searchContainer, searchContainer.getAttribute("phx-hide"))
             }
          }
 
          if(e.metaKey && e.key === 'k'){
-            searchContainer.classList.add("lsb-ease-out");
-            searchContainer.classList.remove("lsb-hidden");
+            this.liveSocket.execJS(searchContainer, searchContainer.getAttribute("phx-show"))
+            searchInput.focus();
          }
 
          if(e.key === 'Escape'){
-            searchContainer.classList.add("lsb-ease-in");
-            searchContainer.classList.add("lsb-hidden");
+            this.liveSocket.execJS(searchContainer, searchContainer.getAttribute("phx-hide"))
          }
       });
 
-      searchContainer.addEventListener('click', (e) => {
-         searchContainer.classList.add("lsb-ease-in");
-         searchContainer.classList.add("lsb-hidden");
+      searchList.addEventListener('click', (e) => {
+         this.liveSocket.execJS(searchContainer, searchContainer.getAttribute("phx-hide"))
       })
 }
  };
