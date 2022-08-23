c3d67aef4c31e2455e8a61a0481e23cb28a663cf
Matthieu Chabert <hey@matthieuchabert.com>
2022-08-23 14:14:04 +0200

    fixed a bunch of visual issues with the keyboard and mouse navigation - improved the overall hover animation

--- assets/js/lib/search_hook.js
+++ assets/js/lib/search_hook.js
@@ -3,30 +3,83 @@ export const SearchHook = {
       const searchContainer = document.querySelector("#search-container");
       const searchList = document.querySelector("#search-list");
       const searchInput = document.querySelector("#search-container input");
+      const allEntries = document.querySelector("#search-list").children
+      const firstEntry = document.querySelector("#search-list").firstElementChild
+      const lastEntry = document.querySelector("#search-list").lastElementChild
+      let activeEntry = firstEntry
 
       window.addEventListener('keydown', (e) => {
-         if(e.key === 'Enter'){
-            if (e.target == document.activeElement) {
-               const link = e.target.children[0]
-               this.pushEventTo("#search-container", "navigate", {path: link.pathname});
-
-               this.liveSocket.execJS(searchContainer, searchContainer.getAttribute("phx-hide"))
-            }
-         }
-
-         if(e.metaKey && e.key === 'k'){
+         if(e.metaKey && (e.key === 'k' || e.key === 'K')){
             this.liveSocket.execJS(searchContainer, searchContainer.getAttribute("phx-show"))
             searchInput.focus();
+
+            activeEntry.classList.add("lsb-bg-indigo-600", "lsb-text-white");
+         }
+      });
+
+      [...allEntries].forEach(entry => {
+         entry.addEventListener('mouseover', (e) => {
+            if (e.movementX != 0 && e.movementY != 0){
+               activeEntry.classList.remove("lsb-bg-indigo-600", "lsb-text-white");
+               activeEntry = e.target
+               activeEntry.classList.add("lsb-bg-indigo-600", "lsb-text-white");
+            }
+         })
+      }); 
+
+      searchContainer.addEventListener('keydown', (e) => {
+         if(e.key === 'Enter'){
+            e.preventDefault();
+            const link = activeEntry.firstElementChild
+
+            this.pushEventTo("#search-container", "navigate", {path: link.pathname});
+            this.liveSocket.execJS(searchContainer, searchContainer.getAttribute("phx-hide"))
          }
 
          if(e.key === 'Escape'){
             this.liveSocket.execJS(searchContainer, searchContainer.getAttribute("phx-hide"))
          }
-      });
+
+         if(e.key === 'Tab'){
+            // This prevents the use of tab within the search modal 
+            // to keep the focus in the search input.
+            e.preventDefault();
+         }
+
+         if(e.key === 'ArrowUp'){
+            activeEntry.classList.remove("lsb-bg-indigo-600", "lsb-text-white");
+
+            if (activeEntry == firstEntry){
+               activeEntry = lastEntry
+            } else {
+               activeEntry = activeEntry.previousElementSibling;
+            }
+
+            activeEntry.classList.add("lsb-bg-indigo-600", "lsb-text-white");
+            activeEntry.scrollIntoView({block: "nearest", inline: "nearest"})
+         }
+
+         if(e.key === 'ArrowDown'){
+            activeEntry.classList.remove("lsb-bg-indigo-600", "lsb-text-white");
+
+            if (activeEntry == lastEntry){
+               activeEntry = firstEntry
+            } else {
+               activeEntry = activeEntry.nextElementSibling;
+            }
+
+            activeEntry.classList.add("lsb-bg-indigo-600", "lsb-text-white");
+            activeEntry.scrollIntoView({block: "nearest", inline: "nearest"})
+         }
+      })
 
       searchList.addEventListener('click', (e) => {
+         const link = activeEntry.firstElementChild
+
+         this.pushEventTo("#search-container", "navigate", {path: link.pathname});
          this.liveSocket.execJS(searchContainer, searchContainer.getAttribute("phx-hide"))
       })
-}
+   }
+
  };
  
