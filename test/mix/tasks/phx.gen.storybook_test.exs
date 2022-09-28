8b96c9f232e6d912ec86e4bdcfab9fa0a4c81139
Christian Blavier <cblavier@gmail.com>
2022-09-28 14:16:39 +0200

    improved storybook generator

--- test/mix/tasks/phx.gen.storybook_test.exs
+++ test/mix/tasks/phx.gen.storybook_test.exs
@@ -13,6 +13,7 @@ defmodule Mix.Tasks.Phx.Gen.StorybookTest do
   @tag :capture_log
   test "mix phx.gen.storybook generates a working storybook", config do
     in_tmp_project(config.test, fn ->
+      for _ <- 1..5, do: send(self(), {:mix_shell_input, :yes?, true})
       Storybook.run([])
 
       [{story, _}] = Code.compile_file("storybook/components/my_component.story.exs")
@@ -23,15 +24,66 @@ defmodule Mix.Tasks.Phx.Gen.StorybookTest do
 
       [{backend, _}] = Code.compile_file("lib/phx_live_storybook_web/storybook.ex")
       assert backend.storybook_path(story) == "/components/my_component"
+      assert backend.config(:otp_app) == :phx_live_storybook_web
+      assert backend.config(:sandbox_class) == "phx-live-storybook-web"
 
       assert_file("assets/js/storybook.js")
-      assert_file("assets/css/storybook.css")
+      assert_file("assets/css/storybook.css", fn file ->
+        assert String.contains?(file, ~s|@import "tailwindcss/base|)
+      end)
 
-      assert_receive {:mix_shell, :info, ["* creating lib/phx_live_storybook_web/storybook.ex"]}
-      assert_receive {:mix_shell, :info, ["* creating storybook/components/my_component.story.exs"]}
-      assert_receive {:mix_shell, :info, ["* creating storybook/my_page.story.exs"]}
-      assert_receive {:mix_shell, :info, ["* creating assets/js/storybook.js"]}
-      assert_receive {:mix_shell, :info, ["* creating assets/css/storybook.css"]}
+      assert_shell_receive :info, ~r|Starting storybook generation|
+      assert_shell_receive :info, ~r|creating lib/phx_live_storybook_web/storybook.ex|
+      assert_shell_receive :info, ~r|creating storybook/components/my_component.story.exs|
+      assert_shell_receive :info, ~r|creating storybook/my_page.story.exs|
+      assert_shell_receive :info, ~r|creating assets/js/storybook.js|
+      assert_shell_receive :info, ~r|creating assets/css/storybook.css|
+      assert_shell_receive :yes?, ~r|Add the following to your.*router.ex.*:|
+      assert_shell_receive :yes?, ~r|Add.*js/storybook.js.*as a new entry point to your esbuild args in .*config/config.exs.*|
+      assert_shell_receive :yes?, ~r|Add a new Tailwind build profile for.*css/storybook.css.*in.*config/config.exs.*|
+      assert_shell_receive :yes?, ~r|Add a new.*endpoint watcher.*for your new Tailwind build profile in.*config/dev.exs.*|
+      assert_shell_receive :yes?, ~r|Add a new.*live_reload pattern.*to your endpoint in.*config/dev.exs.*|
     end)
   end
+
+  @tag :capture_log
+  test "mix phx.gen.storybook --no-tailwind generates a working storybook without tailwind", config do
+    in_tmp_project(config.test, fn ->
+      for _ <- 1..3, do: send(self(), {:mix_shell_input, :yes?, true})
+      Storybook.run(["--no-tailwind"])
+
+      assert_file("storybook/components/my_component.story.exs")
+      assert_file("storybook/my_page.story.exs")
+      assert_file("lib/phx_live_storybook_web/storybook.ex")
+
+      assert_file("assets/js/storybook.js")
+      assert_file("assets/css/storybook.css", fn file ->
+        refute String.contains?(file, ~s|@import "tailwindcss/base|)
+      end)
+
+      assert_shell_receive :info, ~r|Starting storybook generation|
+      assert_shell_receive :info, ~r|creating lib/phx_live_storybook_web/storybook.ex|
+      assert_shell_receive :info, ~r|creating storybook/components/my_component.story.exs|
+      assert_shell_receive :info, ~r|creating storybook/my_page.story.exs|
+      assert_shell_receive :info, ~r|creating assets/js/storybook.js|
+      assert_shell_receive :info, ~r|creating assets/css/storybook.css|
+      assert_shell_receive :yes?, ~r|Add the following to your.*router.ex.*:|
+      assert_shell_receive :yes?, ~r|Add.*js/storybook.js.*as a new entry point to your esbuild args in .*config/config.exs.*|
+      assert_shell_receive :yes?, ~r|Add a new.*live_reload pattern.*to your endpoint in.*config/dev.exs.*|
+    end)
+  end
+
+  test "with wrong flags it fails", config do
+    in_tmp_project(config.test, fn ->
+      assert_raise Mix.Error, "Invalid option: --unknown-flag", fn ->
+        Storybook.run(["--unknown-flag"])
+      end
+    end)
+  end
+
+  defp assert_shell_receive(kind, pattern) do
+    assert_receive {:mix_shell, ^kind, [msg]}
+    assert msg =~ pattern
+  end
+
 end
