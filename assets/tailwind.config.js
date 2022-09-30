222a61c7de05fc010cef289b5665c27a5b109567
Christian Blavier <cblavier@gmail.com>
2022-09-30 14:14:01 +0200

    safelisting base CSS classes for icons

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   content: ["../lib/**/*.{ex,heex}", "./js/**/*.js"],
+  safelist: [
+    { pattern: /^lsb-(w|h|m|p)-.+/ },
+    {
+      pattern:
+        /^lsb-text-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)(-\d\d\d?)?$/,
+    },
+  ],
   theme: {
     extend: {
       minHeight: (theme) => ({
