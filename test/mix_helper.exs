65065e9dcdf962bd841c59c57a1041294ec97c22
Christian Blavier <cblavier@gmail.com>
2023-02-14 14:43:08 +0100

    improved generator to output stories & example for all components

--- test/mix_helper.exs
+++ test/mix_helper.exs
@@ -4,26 +4,7 @@ Mix.shell(Mix.Shell.Process)
 defmodule PhxLiveStorybook.MixHelper do
   import ExUnit.Assertions
 
-  def tmp_path do
-    Path.expand("../tmp", __DIR__)
-  end
-
-  defp random_string(len) do
-    len |> :crypto.strong_rand_bytes() |> Base.encode64() |> binary_part(0, len)
-  end
-
-  def in_tmp(which, function) do
-    tmp_dir = Path.join([tmp_path(), random_string(10)])
-    path = Path.join([tmp_dir, to_string(which)])
-
-    try do
-      File.rm_rf!(path)
-      File.mkdir_p!(path)
-      File.cd!(path, function)
-    after
-      File.rm_rf!(tmp_dir)
-    end
-  end
+  def tmp_path, do: Path.expand("../tmp", __DIR__)
 
   def in_tmp_project(which, function) do
     conf_before = Application.get_env(:phoenix, :generators) || []
@@ -33,23 +14,19 @@ defmodule PhxLiveStorybook.MixHelper do
     try do
       File.rm_rf!(path)
       File.mkdir_p!(path)
-
-      File.cd!(path, fn ->
-        File.touch!("mix.exs")
-        function.()
-      end)
+      File.cd!(path, function)
     after
       File.rm_rf!(tmp_dir)
       Application.put_env(:phoenix, :generators, conf_before)
     end
   end
 
-  def assert_file(file) do
-    assert File.regular?(file), "Expected #{file} to exist, but does not"
+  defp random_string(len) do
+    len |> :crypto.strong_rand_bytes() |> Base.encode64() |> binary_part(0, len)
   end
 
-  def refute_file(file) do
-    refute File.regular?(file), "Expected #{file} to not exist, but it does"
+  def assert_file(file) do
+    assert File.regular?(file), "Expected #{file} to exist, but does not"
   end
 
   def assert_file(file, match) do
