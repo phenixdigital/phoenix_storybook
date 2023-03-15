14b9e76159e4914753a64b56b41812745e7b4063
Christian Blavier <cblavier@gmail.com>
2023-03-15 16:38:36 +0100

    fixed nested modules issue

--- test/fixtures/storybook_content/tree/b_folder/component.story.exs
+++ test/fixtures/storybook_content/tree/b_folder/component.story.exs
@@ -1,4 +1,8 @@
 defmodule TreeStorybook.BFolder.Component do
   use PhoenixStorybook.Story, :component
   def function, do: &Component.component/1
+
+  defmodule NestedStruct do
+    defstruct [:id, :name]
+  end
 end
