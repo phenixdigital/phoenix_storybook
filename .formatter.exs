bde8829acefebed86ad2a35202b05f1ebed25e75
Christian Blavier <cblavier@gmail.com>
2023-09-28 17:01:52 +0200

    exporting formatting locals_without_parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,7 @@
-# Used by "mix format"
+locals_without_parens = [
+  live_storybook: 2
+]
+
 [
   import_deps: [:phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
@@ -7,5 +10,7 @@
     "{config,lib,priv}/**/*.{ex,exs,eex}",
     "test/phoenix_storybook/**/*.{ex,exs}",
     "test/*.{ex,exs}"
-  ]
+  ],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
