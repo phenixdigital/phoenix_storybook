1c842ffe7bee3a7267ece2afbfd429595c2227e6
Christian Blavier <cblavier@gmail.com>
2024-12-03 14:55:03 +0100

    relaxing heroicons dependency

--- mix.exs
+++ mix.exs
@@ -52,7 +52,6 @@ defmodule PhoenixStorybook.MixProject do
       {:phoenix_html_helpers, "~> 1.0"},
       {:phoenix_view, "~> 2.0"},
       {:makeup_eex, "~> 1.0.0"},
-      {:heroicons, "~> 0.5", optional: true},
       {:jason, "~> 1.3", optional: true},
       {:earmark, "~> 1.4"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
