96344d6d32214af7ee02a8b4506f54b25c3f84fc
Roman Chvanikov <roman.chvanikov@owasp.org>
2024-12-18 00:48:47 +0400

    Make source code extraction a bit more sophisticated

--- test/fixtures/components/component.ex
+++ test/fixtures/components/component.ex
@@ -30,6 +30,25 @@ defmodule Component do
     """
   end
 
+  # In tests, we use
+  # "should not appear" keyphrase in comments and docs to
+  # check that the source code is not extracted
+
+  # attrs, comments, and docs that should not appear in the source code
+  attr :index2, :integer, default: 42
+  attr :label2, :string, default: "", doc: "Set your component label"
+
+  @doc """
+  This should not appear in Component.component/1 source code.
+  """
+  def another_component(assigns) do
+    ~H"""
+    <span data-index={@index2}>
+      another_component: <%= @label2 %>
+    </span>
+    """
+  end
+
   @doc """
   Should not be extracted in Component.component/1 source code.
   """
