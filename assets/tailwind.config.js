8c394f2d14aa4b0e533abad030f401b403505c4e
Christian Blavier <cblavier@gmail.com>
2025-07-02 10:50:17 +0200

    removed obsolete Tailwind 3 config

--- assets/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  safelist: [
-    { pattern: /^!?psb-(w|h|m|p)\w?-.+/ },
-    {
-      pattern:
-        /^psb:text-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)(-\d\d\d?)?$/,
-    },
-  ],
-};
