e05611bac455c6de3343489d99ac237090a246f5
Christian Blavier <cblavier@gmail.com>
2025-07-08 15:28:38 +0200

    renamed index -> folder_index

--- test/fixtures/storybook_content/tree/event/_event.index.exs
+++ test/fixtures/storybook_content/tree/event/_event.index.exs
@@ -1,7 +1,7 @@
 defmodule TreeStorybook.Event do
   use PhoenixStorybook.Index
 
-  def index, do: 2
+  def folder_index, do: 2
 
   def entry("event_live_component"), do: [name: "Live Event Component (root)"]
 end
