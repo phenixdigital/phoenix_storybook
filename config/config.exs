6037f54d6a7c76264712cd9fbe0c5fb353950cde
Christian Blavier <cblavier@gmail.com>
2026-01-23 11:02:29 +0100

    fixed elixir warnings

--- config/config.exs
+++ config/config.exs
@@ -6,7 +6,6 @@ config :phoenix_storybook, :env, config_env()
 config :elixir, :dbg_callback, {PhoenixStorybook.Dbg, :debug_fun, [:stdio]}
 
 config :logger,
-  backends: [:console],
   compile_time_purge_matching: [
     [module: Earmark.Parser.LineScanner]
   ]
