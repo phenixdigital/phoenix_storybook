9f3137dee43e45c6c37c0743c9ab8ca7db119fe7
Serhii Fesenko <8006993+SerhiiFesenko@users.noreply.github.com>
2026-06-05 08:51:13 +0300

    chore: require Elixir 1.15

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule PhoenixStorybook.MixProject do
     [
       app: :phoenix_storybook,
       version: @version,
-      elixir: "~> 1.13",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       name: "phoenix_storybook",
