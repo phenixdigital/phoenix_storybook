0e58df2f4b41e6c4b43d84a5102c58233cad5432
Roeland van Batenburg <roeland@pandosearch.com>
2026-07-21 08:55:38 +1200

    fix dialyzer warning in elixir 1.19

--- lib/phoenix_storybook/live/story/playground_preview_live.ex
+++ lib/phoenix_storybook/live/story/playground_preview_live.ex
@@ -70,7 +70,7 @@ defmodule PhoenixStorybook.Story.PlaygroundPreviewLive do
     assign(
       socket,
       variation: variation_or_group,
-      variation_id: if(variation_or_group, do: variation_or_group.id, else: nil),
+      variation_id: variation_or_group.id,
       variations_attributes:
         for variation <- variations, into: %{} do
           {variation_id(variation_or_group, variation.id),
